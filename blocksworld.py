e6e4ba2693089edf1c22c719d62b8850db8682a0
Masataro Asai <guicho2.71828@gmail.com>
2021-08-04 11:17:15 -0400

    removed old designs

--- blocksworld.py
+++ blocksworld.py
@@ -25,63 +25,6 @@ def domain(fn):
 @domain
 class Blocksworld:
 
-    # predicates
-
-    # type annotation styles. these are not good because we can't specify the parameter names.
-    # Wrapping them will end up being redundant.
-    clear:    Callable[[Object], bool]
-    on_table: Callable[[Object], bool]
-    on:       Callable[[Object, Object], bool]
-
-    clear:    Predicate[Object]
-    on_table: Predicate[Object]
-    on:       Predicate[Object, Object]
-
-    # redundant
-    clear:    Predicate[Object("x")]
-    on_table: Predicate[Object("x")]
-    on:       Predicate[Object("x"), Object("y")]
-
-    clear:    Predicate[1]
-    on_table: Predicate[1]
-    on:       Predicate[2]
-
-    # class attribute style
-
-    # redundant
-    clear     = Predicate("x")
-    on_table  = Predicate("x")
-    on        = Predicate("x","y")
-
-    # Sexp style
-    predicates = [
-        ["clear", "x"],
-        ["on_table", "x"],
-        ["on", "x", "y"]
-    ]
-
-    # decorator style. See: inspect.signature(fn).parameters
-    @predicate
-    def clear(x):
-        pass
-    @predicate
-    def on_table(x):
-        pass
-    @predicate
-    def on(x, y):
-        pass
-
-    # constants
-    table : Object
-    
-    table = Object()
-
-    objects = ["table"]
-
-    # actions
-    # type declaration style --- same as predicates, not informative when producing pddls
-    move_b_to_b: Callable[[Object, Object, Object], Action]
-
     # declrator style
     @preconditions
     def move_b_to_b(bm, bf, bt):
