89ada754086d94fbd471bcc3292b21cd051f37db
Masataro Asai <guicho2.71828@gmail.com>
2021-08-13 16:23:29 -0400

    briefcaseworld

--- examples/briefcaseworld.py
+++ examples/briefcaseworld.py
@@ -5,24 +5,31 @@ sys.path.insert(0, '../')
 
 from pddl_in_python import Domain
 
-# If I use type hint notation e.g. m : location,
+# I could use either type hint notation e.g. m : location, or default value e.g. m = locaiton.
 # unfortunately, the current python interpreter looks for a class definition in the read-time.
 
+location = None
+portable = None
+class location:
+    pass
+class portable:
+    pass
 class Briefcaseworld(Domain):
-    def move(m = location, l = location):
+    # def move(m = location, l = location):
+    def move(m : location, l : location):
         if is_at[m]:
             is_at[l] = True
             is_at[m] = False
-            for x in all(portable):
+            for x in all(portable): # current python syntax does not allow annotating loop variable
                 if _in[x]:
                     at[x,l] = True
                     at[x,m] = False
 
-    def take_out(x = portable):
+    def take_out(x : portable):
         if _in[x]:
             _in[x] = False
 
-    def put_in(x = portable, l = location):
+    def put_in(x : portable, l : location):
         if not _in[x] and at[x,l] and is_at[l]:
             _in[x] = True
 
